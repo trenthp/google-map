cf7741ad2f6e4ce8165cdd7aeebba8005152ffec
Trent Petersen <tpetersen@engr-131-29.eastern.ewu.edu>
2014-02-20 14:22:30 -0800

    1st

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+	new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 47.659381, 
+			lon: -117.4236639,
+			zoom: 15
+		}]
+	}).Load(); 
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
